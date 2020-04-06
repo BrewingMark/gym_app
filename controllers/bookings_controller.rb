d0ae3db25e8de7398060901c39774ff6f6f07d46
Mark Nicol <marknicol1991@live.co.uk>
2020-04-06 10:51:40 +0100

    Adds index routes for member, fitness class and booking routes.

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require_relative('../models/booking')
+require_relative('../models/member')
+require_relative('../models/fitness_class')
+also_reload( '../models/*' )
+
+get '/bookings' do
+  erb( :"bookings/index" )
+end
--- controllers/fitness_classes_controller.rb
+++ controllers/fitness_classes_controller.rb
@@ -1,2 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require_relative('../models/fitness_class')
+also_reload( '../models/*' )
+
+get '/fitness_classes' do
+  erb( :"fitness_classes/index" )
+end
