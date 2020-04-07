078db99d218423ddb470c9284f25b1ac138755f2
Mark Nicol <marknicol1991@live.co.uk>
2020-04-07 16:28:02 +0100

    Adds delete path for bookings.

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -9,3 +9,9 @@ get '/bookings' do
   @bookings = Booking.all
   erb( :"bookings/index" )
 end
+
+post '/bookings/:id/delete' do
+  id = params['id'].to_i()
+  Booking.delete(id)
+  redirect to ('/bookings')
+end
